45843a22747c4a164ada4d3abf36a50d28f5a9a9
Zheeeng <air.island.e@foxmail.com>
2021-08-13 15:51:09 +0800

    Update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,3 +4,4 @@ dotenv.config();
 
 export const CRAWLER_ACCESS_TOKEN = process.env.CRAWLER_ACCESS_TOKEN ?? "";
 export const CRAWLER_RUANYIFENG_TOPICS = process.env.CRAWLER_RUANYIFENG_TOPICS?.split(',') ?? []
+export const CRAWLER_PORT = +(process.env.PORT ?? '8868')
