93bf547c392e20e9f4d9287588cbec0f27d8bdbf
Zheeeng <air.island.e@foxmail.com>
2021-08-16 21:28:30 +0800

    Update processors.ts

--- src/crawler/processors.ts
+++ src/crawler/processors.ts
@@ -17,9 +17,15 @@ import {
 
 const filterTimeSpan = 1000 * 3600 * CRAWLER_TIME_SPAN_HOURS;
 
-type ProcessorResult = [total: number, mdContent: string];
+type ProcessorResult = {
+  partial: [frontMatter: string, entries: string[]];
+  all: [frontMatter: string, entries: string[]];
+};
 
 export const ruanYifengBlogProcessor = async (): Promise<ProcessorResult> => {
+  const allFrontMatter = `## 阮一峰技术博客`;
+  const partialFrontMatter = `## 阮一峰技术博客最新博文`;
+
   try {
     const latestArticles = await ruanYifengAllBlogWorker();
 
@@ -31,7 +37,11 @@ export const ruanYifengBlogProcessor = async (): Promise<ProcessorResult> => {
       (article) => article.cTime + filterTimeSpan >= now
     );
 
-    if (!filteredLatestArticles.length) return [0, ""];
+    if (!filteredLatestArticles.length)
+      return {
+        all: [allFrontMatter, []],
+        partial: [partialFrontMatter, []],
+      };
 
     const allForReference = filteredLatestArticles.reduce<
       Record<string, RuanYifengContent>
@@ -42,62 +52,87 @@ export const ruanYifengBlogProcessor = async (): Promise<ProcessorResult> => {
 
     const topics = await Promise.all(
       CRAWLER_RUANYIFENG_TOPICS.map((topic) =>
-        ruanYifengBlogWorker(topic, allForReference)
+        ruanYifengBlogWorker(topic)
       )
     );
-    const totalArticles = topics.flatMap((topic) => topic);
+    const latestTopics = topics.map(topic => topic.filter(article => !!allForReference[article.link]))
 
-    log(`阮一峰博客 ${totalArticles.length} 条`);
+    const totalArticles = topics.flatMap((topic) => topic);
+    const totalLatestArticles = totalArticles.flatMap((topic) => topic);
+
+    log(`阮一峰博客 ${totalArticles.length}/${totalLatestArticles.length} 条`);
 
     topics.forEach((topic, index) => {
       if (CRAWLER_RUANYIFENG_TOPICS[index]) {
         log(
-          `阮一峰博客 ${CRAWLER_RUANYIFENG_TOPICS[index]} 专题 ${topic.length} 条`
+          `阮一峰博客 ${CRAWLER_RUANYIFENG_TOPICS[index]} 专题 ${topic.length}/${latestTopics[index].length} 条`
         );
       }
     });
 
-    const content = [
-      `## 阮一峰技术博客 *Ruan Yifeng's Personal Website*`,
-      ...totalArticles.map((article) => [
+    return {
+      all: [allFrontMatter, totalArticles.map((article) => 
+        `* **[${article.title}](${article.link})**`,
+      )],
+      partial: [partialFrontMatter, totalLatestArticles.map((article) => 
         `* **[${article.title}](${article.link})** *评论数：${article.commentCount}*`,
-      ]),
-    ]
-      .flatMap((i) => i)
-      .join("\n\n");
-
-    return [totalArticles.length, content];
+      )],
+    };
   } catch (error) {
     log(`阮一峰博客 请求错误 ${error.toString()}`, "error");
-    return [0, ""];
+
+    return {
+      all: [allFrontMatter, []],
+      partial: [partialFrontMatter, []],
+    };
   }
 };
 
 export const githubFrontEndTopicProcessor =
   async (): Promise<ProcessorResult> => {
+    const allFrontMatter = `## Github 前端专题榜`;
+    const partialFrontMatter = `## Github 前端专题榜 TOP${CRAWLER_TOP_COUNT}`;
+
     try {
       const articles = await githubFrontEndTopicWorker();
 
       log(`Github 前端专题榜 ${articles.length} 条`);
 
-      const content = [
-        `## Github 前端专题榜 TOP${CRAWLER_TOP_COUNT}`,
-        ...articles.slice(0, CRAWLER_TOP_COUNT).map((article) => [
-          `* **[${article.title}](${article.link})** *${article.language} ｜ ⭐️：${article.stars}*`,
-          `> ${article.content}`,
-        ]),
-      ]
-        .flatMap((i) => i)
-        .join("\n\n");
-
-      return [articles.length, content];
+      return {
+        all: [
+          allFrontMatter,
+          articles
+            .map((article) => [
+              `* **[${article.title}](${article.link})** *${article.language} ｜ ⭐️：${article.stars}*`,
+              `> ${article.content}`,
+            ])
+            .flat(),
+        ],
+        partial: [
+          partialFrontMatter,
+          articles
+            .slice(0, CRAWLER_TOP_COUNT)
+            .map((article) => [
+              `* **[${article.title}](${article.link})** *${article.language} ｜ ⭐️：${article.stars}*`,
+              `> ${article.content}`,
+            ])
+            .flat(),
+        ],
+      };
     } catch (error) {
       log(`Github 前端专题榜请求错误 ${error.toString()}`, "error");
-      return [0, ""];
+
+      return {
+        all: [allFrontMatter, []],
+        partial: [partialFrontMatter, []],
+      };
     }
   };
 
 export const githubTrendingProcessor = async (): Promise<ProcessorResult> => {
+  const allFrontMatter = `## Github TS/JS 今日流行趋势`;
+  const partialFrontMatter = `## Github TS TOP ${CRAWLER_TOP_COUNT}/JS TOP ${CRAWLER_TOP_COUNT} 今日流行趋势`;
+
   try {
     const [githubTrendingTS, githubTrendingJS] = await Promise.all([
       githubTrendingWorker("Typescript"),
@@ -107,26 +142,47 @@ export const githubTrendingProcessor = async (): Promise<ProcessorResult> => {
     log(`Github 趋势榜（Typescript）${githubTrendingTS.length} 条`);
     log(`Github 趋势榜（Javascript）${githubTrendingJS.length} 条`);
 
-    const githubTrending = [...githubTrendingTS.slice(0, CRAWLER_TOP_COUNT), ...githubTrendingJS.slice(0, CRAWLER_TOP_COUNT)];
+    const githubTrending = [...githubTrendingTS, ...githubTrendingJS];
 
-    const content = [
-      `## Github TS TOP ${CRAWLER_TOP_COUNT}/JS TOP ${CRAWLER_TOP_COUNT} 今日流行趋势`,
-      ...githubTrending.map((article) => [
-        `* **[${article.title}](${article.link})** *${article.language} ｜ fork：${article.forks} | ⭐️：${article.stars} | 今日 ⭐️：${article.todayStars}*`,
-        `> ${article.content}`,
-      ]),
-    ]
-      .flatMap((i) => i)
-      .join("\n\n");
+    const partialGithubTrending = [
+      ...githubTrendingTS.slice(0, CRAWLER_TOP_COUNT),
+      ...githubTrendingJS.slice(0, CRAWLER_TOP_COUNT),
+    ];
 
-    return [githubTrending.length, content];
+    return {
+      all: [
+        allFrontMatter,
+        githubTrending
+          .map((article) => [
+            `* **[${article.title}](${article.link})** *${article.language} ｜ fork：${article.forks} | ⭐️：${article.stars} | 今日 ⭐️：${article.todayStars}*`,
+            `> ${article.content}`,
+          ])
+          .flat(),
+      ],
+      partial: [
+        partialFrontMatter,
+        partialGithubTrending
+          .map((article) => [
+            `* **[${article.title}](${article.link})** *${article.language} ｜ fork：${article.forks} | ⭐️：${article.stars} | 今日 ⭐️：${article.todayStars}*`,
+            `> ${article.content}`,
+          ])
+          .flat(),
+      ],
+    };
   } catch (error) {
     log(`Github 趋势榜请求错误 ${error.toString()}`, "error");
-    return [0, ""];
+
+    return {
+      all: [allFrontMatter, []],
+      partial: [partialFrontMatter, []],
+    };
   }
 };
 
 export const juejinHotProcessor = async (): Promise<ProcessorResult> => {
+  const allFrontMatter = `## 掘金前端热贴`;
+  const partialFrontMatter = `## 掘金 24 小时内最新前端热贴`;
+
   try {
     const articles = await juejinFEHotWorker();
 
@@ -138,24 +194,40 @@ export const juejinHotProcessor = async (): Promise<ProcessorResult> => {
 
     log(`掘金前端热帖结果 ${latestArticles.length}/${articles.length} 条`);
 
-    const content = [
-      `## 掘金 24 小时内最新前端热贴`,
-      ...latestArticles.map((article) => [
-        `* **[${article.title}](${article.link})** *作者：${article.authorName} ｜ 评论数：${article.commentCount} | 浏览数：${article.viewCount} | 🧡：${article.diggCount}*`,
-        `> ${article.content}`,
-      ]),
-    ]
-      .flatMap((i) => i)
-      .join("\n\n");
-
-    return [latestArticles.length, content];
+    return {
+      all: [
+        allFrontMatter,
+        articles
+          .map((article) => [
+            `* **[${article.title}](${article.link})** *作者：${article.authorName} ｜ 评论数：${article.commentCount} | 浏览数：${article.viewCount} | 🧡：${article.diggCount}*`,
+            `> ${article.content}`,
+          ])
+          .flat(),
+      ],
+      partial: [
+        partialFrontMatter,
+        latestArticles
+          .map((article) => [
+            `* **[${article.title}](${article.link})** *作者：${article.authorName} ｜ 评论数：${article.commentCount} | 浏览数：${article.viewCount} | 🧡：${article.diggCount}*`,
+            `> ${article.content}`,
+          ])
+          .flat(),
+      ],
+    };
   } catch (error) {
     log(`掘金前端热帖请求错误 ${error.toString()}`, "error");
-    return [0, ""];
+
+    return {
+      all: [allFrontMatter, []],
+      partial: [partialFrontMatter, []],
+    };
   }
 };
 
 export const infoQFEProcessor = async (): Promise<ProcessorResult> => {
+  const allFrontMatter = `## InfoQ 前端之巅前端热贴`;
+  const partialFrontMatter = `## InfoQ 前端之巅 24 小时内最新前端热贴`;
+
   try {
     const articles = await infoQFEWorker();
 
@@ -167,25 +239,40 @@ export const infoQFEProcessor = async (): Promise<ProcessorResult> => {
 
     log(`InfoQ 前端之巅结果 ${latestArticles.length}/${articles.length} 条`);
 
-    const content = [
-      `## InfoQ 前端之巅 24 小时内最新前端热贴`,
-      ...latestArticles.map((article) => [
-        `* **[${article.title}](${article.link})** *作者：${article.authors}*`,
-        `> ${article.content}`,
-      ]),
-    ]
-      .flatMap((i) => i)
-      .join("\n\n");
-
-    return [latestArticles.length, content];
+    return {
+      all: [
+        allFrontMatter,
+        articles
+          .map((article) => [
+            `* **[${article.title}](${article.link})** *作者：${article.authors}*`,
+            `> ${article.content}`,
+          ])
+          .flat(),
+      ],
+      partial: [
+        partialFrontMatter,
+        latestArticles
+          .map((article) => [
+            `* **[${article.title}](${article.link})** *作者：${article.authors}*`,
+            `> ${article.content}`,
+          ])
+          .flat(),
+      ],
+    };
   } catch (error) {
     log(`InfoQ 前端之巅请求错误 ${error.toString()}`, "error");
 
-    return [0, ""];
+    return {
+      all: [allFrontMatter, []],
+      partial: [partialFrontMatter, []],
+    };
   }
 };
 
 export const aliMaMaFeProcessor = async (): Promise<ProcessorResult> => {
+  const allFrontMatter = `## 阿里妈妈前端快爆`;
+  const partialFrontMatter = `## 阿里妈妈前端快爆 24 小时内最新发布`;
+
   try {
     const articles = await aliMaMaFEWorker();
 
@@ -197,20 +284,32 @@ export const aliMaMaFeProcessor = async (): Promise<ProcessorResult> => {
 
     log(`阿里妈妈前端快爆结果 ${latestArticles.length}/${articles.length} 条`);
 
-    const content = [
-      `## 阿里妈妈前端快爆 24 小时内最新发布`,
-      ...latestArticles.map((article) => [
-        `* **[${article.title}](${article.link})** *🧡：${article.voteUp}*`,
-        `> ${article.content}`,
-      ]),
-    ]
-      .flatMap((i) => i)
-      .join("\n\n");
-
-    return [latestArticles.length, content];
+    return {
+      all: [
+        allFrontMatter,
+        articles
+          .map((article) => [
+            `* **[${article.title}](${article.link})** *🧡：${article.voteUp}*`,
+            `> ${article.content}`,
+          ])
+          .flat(),
+      ],
+      partial: [
+        partialFrontMatter,
+        latestArticles
+          .map((article) => [
+            `* **[${article.title}](${article.link})** *🧡：${article.voteUp}*`,
+            `> ${article.content}`,
+          ])
+          .flat(),
+      ],
+    };
   } catch (error) {
     log(`阿里妈妈前端快爆请求错误 ${error.toString()}`, "error");
 
-    return [0, ""];
+    return {
+      all: [allFrontMatter, []],
+      partial: [partialFrontMatter, []],
+    };
   }
 };
