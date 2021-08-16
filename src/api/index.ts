a89aa0c231a6b2dd7bb12d4aa0aa6252f372c224
Zheeeng <air.island.e@foxmail.com>
2021-08-16 14:27:31 +0800

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,7 @@ import { Response, Router } from "farrow-http";
 import { Nullable } from "farrow-schema";
 import { singleton } from "../crawler/singleton";
 import { CRAWLER_ACCESS_TOKEN } from "../config/env";
+import { dingTalkTask } from "../tasks/dingtalkBot";
 
 export const services = Router();
 
@@ -11,26 +12,30 @@ services
     pathname: "/api/fe-daily",
     query: {
       accessToken: String,
-      format: Nullable(String),
     },
   })
   .use(async (request) => {
     if (request.query.accessToken !== CRAWLER_ACCESS_TOKEN)
       return Response.status(401, "Invalid accessToken");
 
-    if (request.query.format === "dingTalk")
-      return Response.json({
-        success: true,
-        errorCode: 200,
-        errorMsg: "",
-        fields: {
-          "msgType": "markdown",
-          "title": "the BIG BANG FE reading",
-          "text": await singleton(),
-          "atDingtalkIds": "对应的钉钉用户钉钉号",
-          "isAtAll": false
-        },
-      });
-
     return Response.text(await singleton());
+  });
+
+// attach todo api
+services
+  .match({
+    pathname: "/dingtalk-bot/fe-daily",
+    query: {
+      accessToken: String,
+      format: Nullable(String),
+    },
+  })
+  .use(async (request) => {
+    if (request.query.accessToken !== CRAWLER_ACCESS_TOKEN)
+      return Response.status(401, "Invalid accessToken");
+    dingTalkTask()
+
+    return Response.json({
+      message: '机器人任务已发布'
+    })
   });
