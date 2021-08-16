0f8927666621caab6dcccfd3103f1bbf2a230110
Zheeeng <air.island.e@foxmail.com>
2021-08-16 14:28:15 +0800

    Update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,6 +3,14 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const CRAWLER_ACCESS_TOKEN = process.env.CRAWLER_ACCESS_TOKEN ?? "";
-export const CRAWLER_RUANYIFENG_TOPICS = process.env.CRAWLER_RUANYIFENG_TOPICS?.split(',') ?? []
-export const CRAWLER_PORT = +(process.env.PORT ?? '8868')
-export const CRAWLER_TIME_SPAN_HOURS = +(process.env.CRAWLER_TIME_SPAN_HOURS ?? '12')
+export const CRAWLER_TOP_COUNT = +(process.env.CRAWLER_TOP_COUNT ?? "5") || 5;
+export const CRAWLER_BAN_TOPICS =
+  process.env.CRAWLER_BAN_TOPICS?.split(",") ?? [];
+export const CRAWLER_RUANYIFENG_TOPICS =
+  process.env.CRAWLER_RUANYIFENG_TOPICS?.split(",") ?? [];
+export const CRAWLER_PORT = +(process.env.PORT ?? "8868");
+export const CRAWLER_TIME_SPAN_HOURS = +(
+  process.env.CRAWLER_TIME_SPAN_HOURS ?? "12"
+);
+export const CRAWLER_DINGTALK_WEBHOOKS =
+  process.env.CRAWLER_DINGTALK_WEBHOOKS?.split(",") ?? [];
